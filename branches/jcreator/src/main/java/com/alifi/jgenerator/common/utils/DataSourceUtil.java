ae2f9976582b6036a42a742133f3eed73df99b33
wdf_doc@163.com <wdf_doc@163.com@462b3b8b-43f8-912e-41aa-f189f24df3a4>
2012-01-05 16:33:13 +0000

    creator

--- branches/jcreator/src/main/java/com/alifi/jgenerator/common/utils/DataSourceUtil.java
+++ branches/jcreator/src/main/java/com/alifi/jgenerator/common/utils/DataSourceUtil.java
@@ -2,11 +2,18 @@ package com.alifi.jgenerator.common.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import javax.sql.DataSource;
 
 public class DataSourceUtil {
 	public final static List<DateSourceMap> dataSourceMaps = new ArrayList<DateSourceMap>();
 
-	public void setDateSourceMap(DateSourceMap dateSourceMap) {
+	public DataSourceUtil(String describe, DataSource dataSource) {
+		DateSourceMap dateSourceMap = new DateSourceMap();
+		dateSourceMap.setDataSource(dataSource);
+		dateSourceMap.setDescribe(describe);
+		dateSourceMap.setKey(UUID.randomUUID().toString());
 		DataSourceUtil.dataSourceMaps.add(dateSourceMap);
 	}
 
